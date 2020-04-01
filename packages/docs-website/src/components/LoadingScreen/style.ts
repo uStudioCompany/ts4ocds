4c2ae0dad6799b3f35b11eea460c078c264bcca3
Dmytro Vasylkivskyi <drizzer14@gmail.com>
2020-04-01 13:28:21 +0300

    feat: Add new Loading screen (#20)
    * feat: Add new Loading screen
    
    * refactor: Remove unused import

--- packages/docs-website/src/components/LoadingScreen/style.ts
+++ packages/docs-website/src/components/LoadingScreen/style.ts
@@ -1,34 +1,46 @@
 import styled, { css } from 'styled-components';
-import LogoIcon from '../../assets/images/ts4ocds-logo.inline.svg';
+import { Flex } from 'ustudio-ui';
 import { withDisplayNames } from '../../utils';
 
-const Logo = styled(LogoIcon)`
-  width: 50%;
-  height: auto;
-`;
-
-const LoadingScreen = styled.div<{ isLoaded: boolean }>(
+const LargeContainer = styled.div<{ isLoaded: boolean }>(
   ({ isLoaded }) => css`
+    display: grid;
+    grid-auto-rows: min-content;
+    grid-gap: var(--i-large);
+
+    opacity: ${isLoaded ? 0 : 1};
+
+    transition: opacity var(--transition);
+
     position: fixed;
     top: 0;
     left: 0;
-    right: 0;
-    bottom: 0;
 
     z-index: calc(var(--l-topmost) * 2);
 
-    display: flex;
-    align-items: center;
-    justify-content: center;
+    width: 100%;
+    height: 100%;
 
-    color: var(--c-lightest);
-    background: var(--g-t4o);
+    padding: calc(var(--i-large) + 49px) var(--i-large) var(--i-large);
+    margin: 0 auto;
+    pointer-events: ${isLoaded ? 'none' : 'auto'};
 
-    opacity: ${isLoaded ? 0 : 1};
-    pointer-events: none;
-
-    transition: opacity var(--transition);
+    background-color: var(--c-lightest);
   `
 );
 
-export default withDisplayNames({ Logo, LoadingScreen });
+const MediumContainer = styled.div`
+  display: grid;
+  grid-auto-rows: min-content;
+  grid-gap: var(--i-regular);
+`;
+
+const InlineContainer = styled(Flex)`
+  margin: 0 calc(var(--i-medium) * -1);
+
+  & > * {
+    margin: 0 var(--i-medium);
+  }
+`;
+
+export default withDisplayNames({ LargeContainer, MediumContainer, InlineContainer });
