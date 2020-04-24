472f50443c801269541bf82d3a179cffa973e72a
drizzer14 <drizzer14@gmail.com>
2020-04-24 15:01:07 +0300

    fix(Website): fix create-pages template paths

--- packages/docs-website/src/lib/create-pages.ts
+++ packages/docs-website/src/lib/create-pages.ts
@@ -8,7 +8,7 @@ import { categories } from './categories';
 const createModulePage = (createPage: Actions['createPage']) => (module: Module) =>
   createPage({
     path: createUrl(module.name),
-    component: resolve('./src/templates/Module/index.ts'),
+    component: resolve('./src/templates/Module/module.component.tsx'),
     context: { ...module },
   });
 
@@ -16,7 +16,7 @@ export const createPages: GatsbyNode['createPages'] = ({ actions: { createPage }
   Object.values(reflectionsMap).forEach((reflection) => {
     createPage({
       path: createUrl(reflection.module, reflection.name),
-      component: resolve('./src/templates/Reflection/index.ts'),
+      component: resolve('./src/templates/Reflection/reflection.component.tsx'),
       context: { ...reflection },
     });
   });
