843ccd8c9acb610a103ba48147804e06e9532867
Dmytro Vasylkivskyi <drizzer14@gmail.com>
2020-04-27 09:44:54 +0300

    feat: Parallelize createPages function (#34)

--- packages/docs-website/src/lib/create-pages.ts
+++ packages/docs-website/src/lib/create-pages.ts
@@ -1,25 +1,36 @@
-import { Actions, GatsbyNode } from 'gatsby';
+import { Actions, GatsbyNode, Page } from 'gatsby';
 import { resolve } from 'path';
 import { reflectionsMap } from '../api';
 import { Module } from '../api/typings';
 import { createUrl } from '../utils';
 import { categories } from './categories';
 
-const createModulePage = (createPage: Actions['createPage']) => (module: Module) =>
-  createPage({
+async function asyncCreatePage<A extends Page>(createPage: Actions['createPage'], args: A): Promise<void> {
+  createPage(args);
+}
+
+const createModulePage = async (createPage: Actions['createPage'], module: Module): Promise<void> => {
+  await asyncCreatePage(createPage, {
     path: createUrl(module.name),
     component: resolve('./src/templates/Module/module.component.tsx'),
     context: { ...module },
   });
+};
 
-export const createPages: GatsbyNode['createPages'] = ({ actions: { createPage } }) => {
-  Object.values(reflectionsMap).forEach((reflection) => {
-    createPage({
-      path: createUrl(reflection.module, reflection.name),
-      component: resolve('./src/templates/Reflection/reflection.component.tsx'),
-      context: { ...reflection },
-    });
-  });
+export const createPages: GatsbyNode['createPages'] = async ({ actions: { createPage } }) => {
+  await Promise.all(
+    Object.values(reflectionsMap).map(async (reflection) => {
+      return asyncCreatePage(createPage, {
+        path: createUrl(reflection.module, reflection.name),
+        component: resolve('./src/templates/Reflection/reflection.component.tsx'),
+        context: { ...reflection },
+      });
+    })
+  );
 
-  Object.values(categories).forEach((modules) => modules.forEach(createModulePage(createPage)));
+  await Promise.all(
+    Object.values(categories).map(async (modules) => {
+      return modules.map(async (module) => createModulePage(createPage, module));
+    })
+  );
 };
